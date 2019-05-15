2c46957dd2fdd7daf8502a27b4e7c0bfbfb15e51
Matthew Rothenberg <mrothenberg@gmail.com>
2019-05-15 13:33:47 -0400

    cmd: finish up early option parsing for PoC

--- cmd/git-semver-describe/main.go
+++ cmd/git-semver-describe/main.go
@@ -12,9 +12,8 @@ import (
 
 var (
 	// flags unique to us...
-	// ...search
-	path = flag.String("path", ".", "path of git repo to describe, otherwise current workdir")
-	// ...formatting
+	// ...main
+	path   = flag.String("path", ".", "path of git repo to describe, otherwise current workdir")
 	legacy = flag.Bool("legacy", false, "display results similar to 'git describe --tags', e.g. not semver compliant")
 
 	// flags compatible with git-describe...
@@ -29,7 +28,7 @@ var (
 	// Some potential additions to implement down the line if there is strong demand:
 	// --match <pattern>     only consider tags matching <pattern>
 	// --exclude <pattern>   do not consider tags matching <pattern>
-	// --dirty[=<mark>]      append <mark> on dirty working tree (default: "-dirty")
+	// --dirty[=<mark>]      append <mark> on dirty working tree (default: "-dirty") // see pflag.NoOptDefVal
 )
 
 func main() {
@@ -44,15 +43,21 @@ func main() {
 		Long:   *long,
 	}
 
-	// TODO: os.args is commitish, not path
-	d, err := describer.DescribePath(*path, "HEAD", opts)
+	commitish := flag.Arg(0)
+	if commitish == "" {
+		commitish = "HEAD"
+	}
+	d, err := describer.DescribePath(*path, commitish, opts)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "fatal: %v.\n", err)
 		os.Exit(128) //??
 	}
 
-	fmt.Println(d.FormatLegacy(formatOpts))
-	fmt.Println(d.Format(formatOpts))
+	if *legacy {
+		fmt.Println(d.FormatLegacy(formatOpts))
+	} else {
+		fmt.Println(d.Format(formatOpts))
+	}
 }
 
 // do a full comparison along with shelling out to git describe to compare its output
